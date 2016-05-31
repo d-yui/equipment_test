7c3755acc70ee31b6fe0c5050089b62b6e486533
d-yui <viruvilu@gmail.com>
2016-06-01 00:36:22 +0900

    dbsetupcomplete

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160531131127) do
+ActiveRecord::Schema.define(version: 20160531150340) do
 
   create_table "equipment", force: :cascade do |t|
     t.string   "name",         limit: 255
