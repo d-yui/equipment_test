c39e7c1731111540e78859131e6dcb09fc805849
d-yui <viruvilu@gmail.com>
2016-06-04 11:07:28 +0900

    pullreq

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Equipment.create!(name:  "Example",
+             model_number: "example",
+             start_date:    "16:00")
+
+
+99.times do |n|
+  name  = Faker::Name.name
+  model_number = "example-#{n+1}"
+  time = "#{n+1}:00"
+  Equipment.create!(name:  name,
+               model_number: model_number,
+               start_date: time)
+end
